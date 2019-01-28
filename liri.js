20c5fbccf6519c7fa4824d756123d754b0be9c20
williamrackley <willrackley65@gmail.com>
2019-01-28 11:02:55 -0500

    added functionality to append output to log.txt file

--- liri.js
+++ liri.js
@@ -5,8 +5,12 @@ var moment = require("moment");
 var keys = require("./keys.js");
 var Spotify = require('node-spotify-api');
 var spotify = new Spotify(keys.spotify);
+var text = process.argv.slice(2);
+var outputText = text.join(' ');
 var action = process.argv[2];
 var userQuestion = process.argv.slice(3);
+var outPutForLog = [];
+var displayForLog;
 
 switch(action){
     case "spotify-this-song": 
@@ -31,24 +35,32 @@ switch(action){
 }
 
 function spotifySearch(){
+    outPutForLog = [];
+    outPutForLog.unshift(outputText);
+    outPutForLog.unshift("\n");
+
     if(process.argv.length === 3){
         userQuestion = "The Sign"
         spotify.search({ type: 'track', query: userQuestion ,limit: 10 })
         .then(function(response) {
-        console.log("==========");
-        console.log("");
+        console.log("\r\n\r\n\r\n");
         
         //artist name
         console.log("Artists: " + response.tracks.items[9].album.artists[0].name);
+        outPutForLog.push("Artists: " + response.tracks.items[9].album.artists[0].name);
         //song name
         console.log("Song Title: " + response.tracks.items[9].name);
+        outPutForLog.push("Song Title: " + response.tracks.items[9].name);
         //preview link
         console.log("Preview Url: " + response.tracks.items[9].preview_url);
+        outPutForLog.push("Preview Url: " + response.tracks.items[9].preview_url);
         //album name
         console.log("Album Title: " + response.tracks.items[9].album.name);
+        outPutForLog.push("Album Title: " + response.tracks.items[9].album.name);
         
-        console.log("");
-        console.log("==========");
+        console.log("\r\n\r\n\r\n");
+        displayForLog = outPutForLog.join("\n ");
+        appendOutput();
     })
     .catch(function(err) {
     console.log(err);
@@ -56,20 +68,25 @@ function spotifySearch(){
     } else {
     spotify.search({ type: 'track', query: userQuestion ,limit: 1 })
     .then(function(response) {
-        console.log("==========");
-        console.log("");
+        console.log("\r\n\r\n\r\n");
 
         //artist name
         console.log("Artists: " + response.tracks.items[0].album.artists[0].name);
+        outPutForLog.push("Artists: " + response.tracks.items[0].album.artists[0].name);
         //song name
         console.log("Song Title: " + response.tracks.items[0].name);
+        outPutForLog.push("Song Title: " + response.tracks.items[0].name);
         //preview link
         console.log("Preview Url: " + response.tracks.items[0].preview_url);
+        outPutForLog.push("Preview Url: " + response.tracks.items[0].preview_url);
         //album name
         console.log("Album Title: " + response.tracks.items[0].album.name);
+        outPutForLog.push("Album Title: " + response.tracks.items[0].album.name);
 
-        console.log("");
-        console.log("==========");
+      console.log("\r\n\r\n\r\n");
+      displayForLog = outPutForLog.join("\n ");
+      appendOutput();
+
     })
     .catch(function(err) {
     console.log(err);
@@ -78,46 +95,71 @@ function spotifySearch(){
 }
 
 function concertSearch(){
+    outPutForLog = [];
+    outPutForLog.unshift(outputText);
+    outPutForLog.unshift("\n");
     axios.get("https://rest.bandsintown.com/artists/" + userQuestion + "/events?app_id=codingbootcamp").then(
     function(response) {
         var venueDate =response.data[0].datetime;
-        console.log("==========");
-        console.log("");
+        console.log("\r\n\r\n\r\n");
+
+        //artist searched
+        console.log("Artist Searched: " + userQuestion);
+        outPutForLog.push("Artist Searched: " + userQuestion);
         //Name of the venue
         console.log("Venue Name: " + response.data[0].venue.name);
+        outPutForLog.push("Venue Name: " + response.data[0].venue.name);
         //Venue location
         console.log("Venue Location: " + response.data[0].venue.city + ", " + response.data[0].venue.country);
+        outPutForLog.push("Venue Location: " + response.data[0].venue.city + ", " + response.data[0].venue.country);
         //Date of the Event (use moment to format this as "MM/DD/YYYY")
         console.log("Event Date: " + moment(venueDate).format("MM/DD/YYYY"));
-        console.log("");
-        console.log("==========");
+        outPutForLog.push("Event Date: " + moment(venueDate).format("MM/DD/YYYY"));
+
+        console.log("\r\n\r\n\r\n");
+        displayForLog = outPutForLog.join("\n ");
+        appendOutput();
     }
 );
 }
 
 function movieSearch(){
+    outPutForLog = [];
+    outPutForLog.unshift(outputText);
+    outPutForLog.unshift("\n");
+
     axios.get("http://www.omdbapi.com/?t=" + userQuestion + "&y=&plot=short&apikey=trilogy").then(
     function(response) {
-        console.log("==========");
-        console.log("");
+        console.log("\r\n\r\n\r\n");
+
         //Title of the movie.
         console.log("Movie Title: " + response.data.Title);
+        outPutForLog.push("Movie Title: " + response.data.Title);
         //Release Date
         console.log("Release Date: " + response.data.Released);
+        outPutForLog.push("Release Date: " + response.data.Released);
         // IMDB Rating of the movie.
         console.log("IMDB Ratinge: " + response.data.imdbRating);
+        outPutForLog.push("IMDB Ratinge: " + response.data.imdbRating);
         //Rotten Tomatoes Rating of the movie.
         console.log("Rotten Tomatoes Ratinge: " + response.data.Ratings[1].Value);
+        outPutForLog.push("Rotten Tomatoes Ratinge: " + response.data.Ratings[1].Value);
         //Country where the movie was produced.
         console.log("Country: " + response.data.Country);
+        outPutForLog.push("Country: " + response.data.Country);
         //Language of the movie.
         console.log("Language: " + response.data.Language);
+        outPutForLog.push("Language: " + response.data.Language);
         //Plot of the movie.
         console.log("Plot: " + response.data.Plot);
+        outPutForLog.push("Plot: " + response.data.Plot);
         //Actors in the movie.
         console.log("Actors: " + response.data.Actors);
-        console.log("");
-        console.log("==========");
+        outPutForLog.push("Actors: " + response.data.Actors);
+
+        console.log("\r\n\r\n\r\n");
+        displayForLog = outPutForLog.join("\n ");
+        appendOutput();
     }
     );
 }
@@ -150,8 +192,21 @@ function doWhatItSays(){
             movieSearch();
             break;           
       }
-    });
-     
-    
-    
+    }); 
+}
+
+function appendOutput(){
+    fs.appendFile("log.txt", displayForLog, function(err) {
+
+        // If an error was experienced we will log it.
+        if (err) {
+          console.log(err);
+        }
+      
+        // If no error is experienced, we'll log this phrase to our node console.
+        else {
+          console.log("Output Added To log.txt");
+        }
+      
+      });
 }
